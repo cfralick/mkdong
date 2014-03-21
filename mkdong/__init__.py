559cda8874219eca806780f0821f573d52e9bc80
Clint Fralick <cfralick@pandemiclabs.com>
2014-03-21 11:28:56 -0400

    revised dong-creating function, added cli options & argparse

---
+++ mkdong/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+cli utility that makes dongs.
+"""
+
+from mkdong import main as mkdong
---
+++ mkdong/mkdong.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+import os
+import sys
+import argparse
+
+"""
+Makes dongs.
+"""
+
+__author__ = 'Jathan McCollum, Mark Ellzey Thomas'
+__maintainer__ = 'Jathan McCollum'
+__email__ = 'jathan@gmail.com'
+__copyright__ = '2009-2013, Jathan McCollum'
+__version__ = '5.0'
+
+MAXLEN = os.environ.get('MEGADONG') or 40
+
+BALLS = '( )/( )'
+HEAD = 'D'
+CLIMAX='~~~~'
+
+def mkdong(length, climax):
+    """Print a dong of ``length`` length."""
+    if not isinstance(length, int):
+        raise TypeError("error: %s is not a valid length" % length)
+    elif length > MAXLEN:
+        raise ValueError("error: a %s\" dong is too big! "
+                         "cannot be longer than %s\"!" %
+                         (length, MAXLEN))
+    
+    shaft = []
+    for i in xrange(length):
+        shaft.append('/')
+    dong = [BALLS, ''.join(shaft), HEAD]
+    if climax:
+        dong.append(CLIMAX)
+
+    return ''.join(dong)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Make a dong.',
+        prog='mkdong',
+        epilog='mkdong %s' % __version__
+    )
+    parser.add_argument('length', 
+                        metavar='l', 
+                        type=int, 
+                        help='the desired dong length')
+    
+    parser.add_argument('-c', 
+                        '--climax', 
+                        action='store_true',
+                        help='make a climaxing dong')
+
+    parser.add_argument('-v',
+                        '--version',
+                        action='version',
+                        version='%(prog)s ' + __version__,
+                        help='print version and exit')
+    
+    
+    dong_args = parser.parse_args()
+    donglen = dong_args.length
+    climax = dong_args.climax
+    
+    try:
+        dong = mkdong(donglen, climax)
+    except TypeError as fail:
+        sys.exit(str(fail))
+    except ValueError as ronjeremy:
+        sys.exit(str(ronjeremy))
+    else:
+        os.system('echo "%s"' % dong)
+
+
+if __name__ == '__main__':
+    main()
