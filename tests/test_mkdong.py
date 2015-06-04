e18b43e3003ff3d9ca953e2e49224dfdd2832401
Clint Fralick <cfralick@creeperengine.com>
2015-06-03 23:27:24 -0400

    Update parser and main script
    This simplifies the argument parser and adds redesigned
    dong presentation features:
    * set dong girth with --thick and --thin command-line options
    * specify the climax size with --climax

--- tests/test_mkdong.py
+++ tests/test_mkdong.py
@@ -1,46 +1,53 @@
+
+from __future__ import absolute_import
+
 import unittest
-from mkdong.mkdong import mkdong, MAXLEN
+from mkdong.dong import Defaults
+from mkdong.mkdong import parser
 from argparse import ArgumentError
 
 class MkdongTestCase(unittest.TestCase):
+    
+    @classmethod
+    def setUpClass(cls):
+        return None
 
     def setUp(self):
-        self.default_dong = mkdong(1, False)
-        self.maxlen = MAXLEN
-        self.dong_too_long = MAXLEN + 1
+        pass
     
     def tearDown(self):
-        self.default_dong = None
-        self.maxlen = 0
-        self.dong_too_long = None
+        pass
 
     def test_dong_is_dong(self):
         dong = '( )/( )=D'
-        self.assertEqual(mkdong(1, False), dong)
-    
+        args = parser(Defaults).parse_args('1'.split())
+        self.assertEqual(args.func(args), dong)
+
+    def test_dong_is_as_long_as_specified_dong_length(self):
+        dong = '( )/( )==========D'
+        args = parser(Defaults).parse_args('10'.split())
+        self.assertEqual(args.func(args), dong)
+
+
+    def test_climaxing_dong_has_climax(self):
+        dong = '( )/( )=D~'
+        args = parser(Defaults).parse_args('1 -c'.split())
+        self.assertEqual(args.func(args), dong)
+
+    def test_thin_dong_is_thin(self):
+        dong = '( )/( )-----D'
+        args = parser(Defaults).parse_args('5 --thin'.split())
+        self.assertEqual(args.func(args), dong)
+
     def test_wide_dong_is_wide(self):
-        dong = '( )/( )/D'
-        self.assertEqual(mkdong(1, True), dong)
+        dong = '( )/( )/////D'
+        args = parser(Defaults).parse_args('5 --wide'.split())
+        self.assertEqual(args.func(args), dong)
 
-    def test_microdong_is_still_dong(self):
-        micro = '( )/( )D'
-        self.assertEqual(mkdong(0, False), micro)
+    def test_wide_dong_can_climax(self):
+        self.assertTrue(True)
 
-    def test_wide_thin_microdongs_same(self):
-        self.assertEqual(mkdong(0, False), mkdong(0, True))
-
-    
-    def test_climaxing_dong_shows_load(self):
-        self.assertEqual(mkdong(1, False, True), self.default_dong + ' ~~~~')
-
-    def test_non_int_dong_length_raises_type_error(self):
-        with self.assertRaises(TypeError):
-            mkdong('penis', True)
-    
-    def test_superfluous_int_argument_raises_type_error(self):
-        with self.assertRaises(TypeError):
-            mkdong(10, False, False, 20)
-
-    def test_superfluous_str_argument_raises_type_error(self):
-        with self.assertRaises(TypeError):
-            mkdong(10, False, False, 'cock')
+    def test_even_thin_dong_can_climax(self):
+        dong = '( )/( )-D~'
+        args = parser(Defaults).parse_args('1 --thin -c'.split())
+        self.assertEqual(args.func(args), dong)
