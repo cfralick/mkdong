0149ed197abd4179389201723078ac320a39408d
Clint Fralick <cfralick@creeperengine.com>
2015-06-27 21:10:00 -0400

    Remove old test suite
    This removes the previous test suite, which has been obsoleted by the
    rewrite of the mkdong module.
    
    TODO: write replacement test suite

--- tests/test_mkdong.py
+++ tests/test_mkdong.py
@@ -1,49 +1,19 @@
-import unittest
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
 import sys
-from mkdong.mkdong import mkdong, MAXLEN
+from mkdong.mkdong import Dong
 from argparse import ArgumentError
 
-class MkdongTestCase(unittest.TestCase):
+class DongTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.default_dong = mkdong(1, False)
-        self.maxlen = MAXLEN
-        self.dong_too_long = MAXLEN + 1
+        self.dong = Dong
 
     def tearDown(self):
-        self.default_dong = None
-        self.maxlen = 0
-        self.dong_too_long = None
+        self.dong = None
 
     def test_dong_is_dong(self):
-        dong = '( )/( )=D'
-        self.assertEqual(mkdong(1, False), dong)
-
-    def test_wide_dong_is_wide(self):
-        dong = '( )/( )/D'
-        self.assertEqual(mkdong(1, True), dong)
-
-    def test_microdong_is_still_dong(self):
-        micro = '( )/( )D'
-        self.assertEqual(mkdong(0, False), micro)
-
-    def test_wide_thin_microdongs_same(self):
-        self.assertEqual(mkdong(0, False), mkdong(0, True))
-
-    def test_climaxing_dong_shows_load(self):
-        self.assertEqual(mkdong(1, False, True), self.default_dong + ' ~~~~')
-    
-    @unittest.skipIf(sys.version_info.minor == 6, "not supported in {}".format(sys.version))
-    def test_non_int_dong_length_raises_type_error(self):
-        with self.assertRaises(TypeError):
-            mkdong('penis', True)
-
-    @unittest.skipIf(sys.version_info.minor == 6, "not supported in {}".format(sys.version))
-    def test_superfluous_int_argument_raises_type_error(self):
-        with self.assertRaises(TypeError):
-            mkdong(10, False, False, 20)
-
-    @unittest.skipIf(sys.version_info.minor == 6, "not supported in {}".format(sys.version))
-    def test_superfluous_str_argument_raises_type_error(self):
-        with self.assertRaises(TypeError):
-            mkdong(10, False, False, 'cock')
+        self.assertTrue(True)
